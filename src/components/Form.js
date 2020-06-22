88f14263aec592f6b2717fd8d22cad05b2379c3e
iptmh <tammyip@gmail.com>
2020-06-21 23:44:24 -0700

    update

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,25 +1,38 @@
 import React from "react";
-
 function Form(props) {
   return (
     <div className={props.className} style={props.style}>
-      <div className="url-entry">
-        <label>Enter API URL: </label>
-        <input type="text" value={props.url} onChange={props.onURLChange} />
+      <div className='url-entry'>
+        <label>API URL:</label>
+        <input
+          type='text'
+          value={props.url}
+          onChange={props.onURLChange}
+        />
       </div>
-
-      <div className="method-select">
-        <p> Choose REST method:</p>
+      <div className='method-select'>
         <select onChange={props.onMethodChange}>
-          <option value="GET">Get</option>
-          <option value="POST">Post</option>
-          <option value="PUT">Put</option>
-          <option value="DELETE">Delete</option>
-          <option value="PATCH">Patch</option>
+          <option value='GET'>Get</option>
+          <option value='POST'>Post</option>
+          <option value='PUT'>Put</option>
+          <option value='DELETE'>Delete</option>
         </select>
       </div>
-      <button id="submit-btn"
-        onClick={props.onSubmit}>Submit</button>
+      <div className='body-entry'>
+        <label>BODY:</label>
+        <textarea
+          value={props.body}
+          onChange={props.onBodyChange}
+        ></textarea>
+      </div>
+      <div className='header-entry'>
+        <label>HEADERS:</label>
+        <textarea
+          value={props.headers}
+          onChange={props.onHeadersChange}
+        ></textarea>
+      </div>
+      <button onClick={props.onSubmit}>Submit</button>
     </div>
   );
 }
